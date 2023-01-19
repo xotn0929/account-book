943465be3397fa94770842f6d6d599a834c9a5a4
xotn0929 <108454757+xotn0929@users.noreply.github.com>
2023-01-19 19:42:49 +0900

    create project

---
+++ accountbook/src/test/java/com/business/accountbook/AccountbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.accountbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
