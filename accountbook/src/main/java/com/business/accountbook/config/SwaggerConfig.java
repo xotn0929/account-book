1db4e43cd9f6f7d5adabfe2c19a49daf0dd94138
xotn0929 <108454757+xotn0929@users.noreply.github.com>
2023-01-21 03:04:25 +0900

    create swagger config

---
+++ accountbook/src/main/java/com/business/accountbook/config/SwaggerConfig.java
@@ -0,0 +1,60 @@
+package com.business.accountbook.config;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .useDefaultResponseMessages(false)
+            .consumes(getConsumeContentTypes())
+            .produces(getProduceContentTypes())
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.business.accountbook"))
+            .paths(PathSelectors.any())
+            .build()
+            .genericModelSubstitutes(Mono.class, Flux.class);
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumeContentTypes = new HashSet<>();
+
+        consumeContentTypes.add("application/json;charset=UTF-8");
+        consumeContentTypes.add("application/x-www-form-urlencoded");
+
+        return consumeContentTypes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produceContentTypes = new HashSet<>();
+
+        produceContentTypes.add("application/json;charset=UTF-8");
+
+        return produceContentTypes;
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Account Book API")
+            .description("Account Book API")
+            .build();
+    }
+}
