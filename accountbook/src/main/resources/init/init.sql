8e63fb3f55e14795524d5ed4976a35df2c18283e
xotn0929 <108454757+xotn0929@users.noreply.github.com>
2023-01-25 04:42:28 +0900

    user ddl update

--- accountbook/src/main/resources/init/init.sql
+++ accountbook/src/main/resources/init/init.sql
@@ -1,24 +1,12 @@
-/* CREATE USER 'acb'@'localhost' IDENTIFIED BY 'test';
-CREATE USER 'acb'@'%' IDENTIFIED BY 'test';
-
-
-CREATE DATABASE accountbook DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; */
-
-
-/* FLUSH PRIVILEGES; */
-/* GRANT ALL ON accountbook.* TO 'acb'@'localhost';
-GRANT ALL ON accountbook.* TO 'acb'@'*';
-
-USE accountbook;
- */
 DROP TABLE IF EXISTS TB_USER;
 CREATE TABLE TB_USER(
     id BINARY(16) PRIMARY KEY COMMENT '기본키',
-    user_id VARCHAR(50) NOT NULL COMMENT '사용자 아이디',
-    user_pw VARCHAR(1000) NOT NULL COMMENT '사용자 비밀번호',
+    user_name VARCHAR(50) NOT NULL COMMENT '사용자 아이디',
+    password VARCHAR(1000) NOT NULL COMMENT '사용자 비밀번호',
     token VARCHAR(500) COMMENT '재인증 토큰',
     create_date TIMESTAMP COMMENT '생성일시',
-    update_date TIMESTAMP COMMENT '수정일시'
+    update_date TIMESTAMP COMMENT '수정일시',
+    roles VARCHAR(100) COMMENT '권한'
 ) COMMENT '회원' DEFAULT charset=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 
